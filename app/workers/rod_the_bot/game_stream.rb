d129b53b2c080a33331a6a0db839d591be240a75
H. Wade Minter <minter@lunenburg.org>
2023-10-25 20:26:02 -0400

    use PERIOD_END instead of PERIOD_OFFICIAL for in-game

--- app/workers/rod_the_bot/game_stream.rb
+++ app/workers/rod_the_bot/game_stream.rb
@@ -26,7 +26,7 @@ module RodTheBot
             RodTheBot::GameStartWorker.perform_async(@game_id)
             REDIS.set("#{game_id}:#{play["about"]["eventId"]}", "true", ex: 172800)
           end
-        elsif play["result"]["eventTypeId"] == "PERIOD_OFFICIAL"
+        elsif play["result"]["eventTypeId"] == "PERIOD_END"
           if REDIS.get("#{@game_id}:#{play["about"]["eventId"]}").nil?
             RodTheBot::EndOfPeriodWorker.perform_async(@game_id, play["about"]["ordinalNum"]) unless play["about"]["periodType"] == "SHOOTOUT"
             REDIS.set("#{game_id}:#{play["about"]["eventId"]}", "true", ex: 172800)
