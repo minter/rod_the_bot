39beca35582d5ac5320aaf3e479e95ad8fe69bde
H. Wade Minter <minter@lunenburg.org>
2023-11-11 17:52:30 -0500

    Convert PeriodStartWorker

--- app/workers/rod_the_bot/period_start_worker.rb
+++ app/workers/rod_the_bot/period_start_worker.rb
@@ -1,16 +1,17 @@
 module RodTheBot
   class PeriodStartWorker
     include Sidekiq::Worker
+    include ActiveSupport::Inflector
 
     def perform(game_id, period_number)
-      @feed = HTTParty.get("https://statsapi.web.nhl.com/api/v1/game/#{game_id}/feed/live")
-      @home = @feed["liveData"]["linescore"]["teams"]["home"]["team"]
-      @visitor = @feed["liveData"]["linescore"]["teams"]["away"]["team"]
+      @feed = HTTParty.get("https://api-web.nhle.com/v1/gamecenter/#{game_id}/play-by-play")
+      home = @feed["homeTeam"]
+      away = @feed["awayTeam"]
 
       post = <<~POST
-        🎬 It's time to start the #{period_number} period at #{@feed["gameData"]["venue"]["name"]}!
+        🎬 It's time to start the #{ordinalize(period_number)} period at #{@feed["venue"]["default"]}!
 
-        We're ready for another puck drop between the #{@visitor["name"]} and the #{@home["name"]}!
+        We're ready for another puck drop between the #{away["name"]["default"]} and the #{home["name"]["default"]}!
       POST
 
       RodTheBot::Post.perform_async(post)
