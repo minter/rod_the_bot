d760ed311fa6634162d2ed051ca940a6f8f8ef21
H. Wade Minter <minter@lunenburg.org>
2023-11-11 16:27:01 -0500

    Migrate YesterdaysScoresWorker to new API

--- app/workers/rod_the_bot/yesterdays_scores_worker.rb
+++ app/workers/rod_the_bot/yesterdays_scores_worker.rb
@@ -4,21 +4,23 @@ module RodTheBot
 
     def perform
       # Get the scores for all games last night
-      yesterday = Date.yesterday
+      yesterday = Date.yesterday.strftime("%Y-%m-%d")
       scores_post = "🙌 Final scores from last night's games:\n\n"
-      response = HTTParty.get("https://statsapi.web.nhl.com/api/v1/schedule?date=#{yesterday.strftime("%Y-%m-%d")}&expand=schedule.linescore")
-      if response["dates"].empty? || response["dates"][0]["games"].empty?
+      response = HTTParty.get("https://api-web.nhle.com/v1/score/#{yesterday}")["games"]
+      yesterday_scores = response.find_all { |game| game["gameDate"] == yesterday }
+      scores = []
+      if yesterday_scores.empty?
         scores_post += "No games scheduled\n"
       else
-        scores = response["dates"][0]["games"].map do |game|
-          home_team = HTTParty.get("https://statsapi.web.nhl.com#{game["teams"]["home"]["team"]["link"]}").parsed_response["teams"][0]["abbreviation"]
-          home_score = game["teams"]["home"]["score"]
-          visitor_team = HTTParty.get("https://statsapi.web.nhl.com#{game["teams"]["away"]["team"]["link"]}").parsed_response["teams"][0]["abbreviation"]
-          visitor_score = game["teams"]["away"]["score"]
-          score = "#{visitor_team} #{visitor_score} : #{home_score} #{home_team}"
-          score += " (SO)" if game["linescore"]["currentPeriodOrdinal"] == "SO"
-          score += " (OT)" if game["linescore"]["currentPeriodOrdinal"] == "OT"
-          score
+        yesterday_scores.each do |game|
+          home_team = game["homeTeam"]
+          home_score = home_team["score"]
+          visitor_team = game["awayTeam"]
+          visitor_score = visitor_team["score"]
+          score_text = "#{visitor_team["abbrev"]} #{visitor_score} : #{home_score} #{home_team["abbrev"]}"
+          score_text += " (SO)" if game["periodDescriptor"]["periodType"] == "SO"
+          score_text += " (OT)" if game["periodDescriptor"]["periodType"] == "OT"
+          scores.push(score_text)
         end
 
         # Save the scores in a post
