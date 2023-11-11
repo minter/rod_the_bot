66ee5debf356e9b8cdc02b2bc80e89e2d458ec55
H. Wade Minter <minter@lunenburg.org>
2023-11-11 16:06:18 -0500

    Convert DivisionsStandingsWorker to the new API

--- app/workers/rod_the_bot/division_standings_worker.rb
+++ app/workers/rod_the_bot/division_standings_worker.rb
@@ -2,24 +2,19 @@ module RodTheBot
   class DivisionStandingsWorker
     include Sidekiq::Worker
 
-    def perform(team_id)
-      # Get the team's division ID
-      team = HTTParty.get("https://statsapi.web.nhl.com/api/v1/teams/#{team_id}").parsed_response["teams"][0]
-      division_id = team["division"]["id"]
+    def perform
+      # Get the standings for your team's division
+      standings = HTTParty.get("https://api-web.nhle.com/v1/standings/now")["standings"]
+      my_team = standings.find { |team| team["teamAbbrev"]["default"] == ENV["NHL_TEAM_ABBREVIATION"] }
+      my_division = my_team["divisionName"]
+      division_teams = standings.select { |team| team["divisionName"] == my_division }.sort_by { |team| [team["points"], team["gamesPlayed"]] }.reverse
 
       # Start post
-      post = "📋 Here are the current standings for the #{team["division"]["name"]} division:\n\n"
-
-      # Get the standings for the team's division
-      standings = HTTParty.get("https://statsapi.web.nhl.com/api/v1/standings/byDivision?division=#{division_id}").parsed_response["records"][0]["teamRecords"]
-
-      # Sort the standings by points from highest to lowest
-      standings.sort_by! { |team| -team["points"] }
+      post = "📋 Here are the current standings for the #{my_division} division:\n\n"
 
       # Print the team abbreviation, position, and points for each team in the standings
-      standings.each_with_index do |team, index|
-        team_data = HTTParty.get("https://statsapi.web.nhl.com/api/v1/teams/#{team["team"]["id"]}").parsed_response["teams"][0]
-        post += "#{index + 1}. #{team_data["abbreviation"]}: #{team["points"]} pts (#{team["gamesPlayed"]} GP)\n"
+      division_teams.each_with_index do |team, index|
+        post += "#{index + 1}. #{team["teamAbbrev"]["default"]}: #{team["points"]} pts (#{team["gamesPlayed"]} GP)\n"
       end
 
       RodTheBot::Post.perform_async(post)
