d5c8618503e1fe99266a468a61ed4f8c87a57ddf
H. Wade Minter <minter@lunenburg.org>
2023-11-29 21:19:16 -0500

    Try reenabling workflow

--- config/initializers/bsky.rb
+++ config/initializers/bsky.rb
@@ -1,3 +1,5 @@
-credentials = Bskyrb::Credentials.new(ENV["BLUESKY_USERNAME"], ENV["BLUESKYU_APP_PASSWORD"])
-session = Bskyrb::Session.new(credentials, ENV["BLUESKY_URL"])
-@bsky = Bskyrb::RecordManager.new(session)
+unless Rails.env.test?
+  credentials = Bskyrb::Credentials.new(ENV["BLUESKY_USERNAME"], ENV["BLUESKYU_APP_PASSWORD"])
+  session = Bskyrb::Session.new(credentials, ENV["BLUESKY_URL"])
+  @bsky = Bskyrb::RecordManager.new(session)
+end
