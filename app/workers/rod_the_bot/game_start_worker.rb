c7c4138fba9d46b5fee3c58ea173fb46a736ac92
H. Wade Minter <wade@wademinter.com>
2024-04-27 23:54:34 -0400

    Starting goalies are back!

--- app/workers/rod_the_bot/game_start_worker.rb
+++ app/workers/rod_the_bot/game_start_worker.rb
@@ -4,13 +4,12 @@ module RodTheBot
 
     def perform(game_id)
       @feed = fetch_data("https://api-web.nhle.com/v1/gamecenter/#{game_id}/play-by-play")
-      # players = build_players(@feed)
-      # home_goalie = find_starting_goalie(@feed["homeTeam"], players)
-      # home_goalie_record = find_goalie_record(home_goalie[:id])
-      # away_goalie = find_starting_goalie(@feed["awayTeam"], players)
-      # away_goalie_record = find_goalie_record(away_goalie[:id])
+      home_goalie = find_starting_goalie("homeTeam")
+      home_goalie_record = find_goalie_record(home_goalie["playerId"])
+      away_goalie = find_starting_goalie("awayTeam")
+      away_goalie_record = find_goalie_record(away_goalie["playerId"])
       officials = find_officials(game_id)
-      post = format_post(@feed, officials)
+      post = format_post(@feed, officials, home_goalie, home_goalie_record, away_goalie, away_goalie_record)
       RodTheBot::Post.perform_async(post)
     end
 
@@ -20,16 +19,14 @@ module RodTheBot
       HTTParty.get(url)
     end
 
-    def find_starting_goalie(team, players)
-      team["onIce"].each do |player|
-        id = player["playerId"]
-        return players[id] if players.fetch(id, {})[:position] == "G"
-      end
+    def find_starting_goalie(team)
+      @feed["summary"]["iceSurface"][team]["goalies"].first
     end
 
     def find_goalie_record(player_id)
+      season = (@feed["gameType"] == 3) ? "playoffs" : "regularSeason"
       player = fetch_data("https://api-web.nhle.com/v1/player/#{player_id}/landing")
-      stats = player["featuredStats"]["regularSeason"]["subSeason"]
+      stats = player["featuredStats"][season]["subSeason"]
       "(#{stats["wins"]}-#{stats["losses"]}-#{stats["otLosses"]}, #{sprintf("%.2f", stats["goalsAgainstAvg"].round(2))} GAA, #{sprintf("%.3f", stats["savePctg"].round(3))} SV%)"
     end
 
@@ -41,27 +38,17 @@ module RodTheBot
       officials
     end
 
-    def format_post(feed, officials)
+    def format_post(feed, officials, home_goalie, home_goalie_record, away_goalie, away_goalie_record)
       <<~POST
         🚦 It's puck drop at #{feed["venue"]["default"]} for #{feed["awayTeam"]["name"]["default"]} at #{feed["homeTeam"]["name"]["default"]}!
         
+        Starting Goalies:
+        #{feed["homeTeam"]["abbrev"]}: ##{home_goalie["sweaterNumber"]} #{home_goalie["name"]["default"]} #{home_goalie_record}
+        #{feed["awayTeam"]["abbrev"]}: ##{away_goalie["sweaterNumber"]} #{away_goalie["name"]["default"]} #{away_goalie_record}
+
         Refs: #{officials[:referees].map { |r| r["default"] }.join(", ")}
         Lines: #{officials[:lines].map { |r| r["default"] }.join(", ")}
       POST
-    end
-
-    def build_players(feed)
-      players = {}
-      feed["rosterSpots"].each do |player|
-        players[player["playerId"]] = {
-          team_id: player["teamId"],
-          number: player["sweaterNumber"],
-          position: player["positionCode"],
-          name: "#{player["firstName"]["default"]} #{player["lastName"]["default"]}",
-          id: player["playerId"]
-        }
-      end
-      players
     end
   end
 end
