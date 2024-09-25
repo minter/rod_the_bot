9eaec8d918dfbd920a6101dc244ed1432c232941
H. Wade Minter <wade@wademinter.com>
2024-09-24 20:47:13 -0500

    Update player name in three stars data

--- app/workers/rod_the_bot/three_stars_worker.rb
+++ app/workers/rod_the_bot/three_stars_worker.rb
@@ -61,7 +61,7 @@ module RodTheBot
     end
 
     def format_player_info(player, stats)
-      "#{player["teamAbbrev"]} ##{player["sweaterNo"]} #{player["name"]} #{stats}\n"
+      "#{player["teamAbbrev"]} ##{player["sweaterNo"]} #{player["name"]["default"]} #{stats}\n"
     end
 
     def post_three_stars(post)
