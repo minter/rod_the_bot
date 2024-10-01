ecd038cf7fc2658520860f37a51c6570eb1fa9b6
H. Wade Minter <wade@wademinter.com>
2024-10-01 13:33:17 -0400

    Fix issue with division standings worker test

--- test/workers/rod_the_bot/division_standings_worker_test.rb
+++ test/workers/rod_the_bot/division_standings_worker_test.rb
@@ -1,11 +1,6 @@
 require "test_helper"
 
-VCR.configure do |config|
-  config.cassette_library_dir = "fixtures/vcr_cassettes"
-  config.hook_into :webmock
-end
-
-class RodTheBot::DivisionStandingsWorkerTest < Minitest::Test
+class RodTheBot::DivisionStandingsWorkerTest < ActiveSupport::TestCase
   def setup
     ENV["NHL_TEAM_ABBREVIATION"] = "CAR"
     Sidekiq::Worker.clear_all
@@ -14,25 +9,24 @@ class RodTheBot::DivisionStandingsWorkerTest < Minitest::Test
 
   def test_perform
     VCR.use_cassette("nhl_standings_now", allow_playback_repeats: true) do
-      NhlApi.stub :preseason?, false do
-        @worker.perform
-        assert_equal 1, RodTheBot::Post.jobs.size
+      NhlApi.stubs(:preseason?).returns(false)
+      @worker.perform
+      assert_equal 1, RodTheBot::Post.jobs.size
 
-        expected_output = <<~POST
-          📋 Here are the current standings for the Metropolitan division (by PT%):
+      expected_output = <<~POST
+        📋 Here are the current standings for the Metropolitan division (by PT%):
 
-          1. NYR: 31 pts (0.775%)
-          2. CAR: 26 pts (0.619%)
-          3. WSH: 22 pts (0.611%)
-          4. NJD: 21 pts (0.525%)
-          5. NYI: 22 pts (0.524%)
-          6. PHI: 23 pts (0.523%)
-          7. PIT: 21 pts (0.500%)
-          8. CBJ: 18 pts (0.391%)
-        POST
+        1. NYR: 31 pts (0.775%)
+        2. CAR: 26 pts (0.619%)
+        3. WSH: 22 pts (0.611%)
+        4. NJD: 21 pts (0.525%)
+        5. NYI: 22 pts (0.524%)
+        6. PHI: 23 pts (0.523%)
+        7. PIT: 21 pts (0.500%)
+        8. CBJ: 18 pts (0.391%)
+      POST
 
-        assert_equal expected_output, RodTheBot::Post.jobs.first["args"].first
-      end
+      assert_equal expected_output, RodTheBot::Post.jobs.first["args"].first
     end
   end
 
