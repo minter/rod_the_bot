6636eff15698169329f0d18d1542a5db08c68fcd
H. Wade Minter <wade@wademinter.com>
2024-09-30 22:56:15 -0400

    Update period start worker tests

--- test/workers/period_start_worker_test.rb
+++ test/workers/period_start_worker_test.rb
@@ -1,66 +1,68 @@
-require "minitest/autorun"
+require "test_helper"
 require "vcr"
 
-VCR.configure do |config|
-  config.cassette_library_dir = "fixtures/vcr_cassettes"
-  config.hook_into :webmock
-end
-
-class PeriodStartWorkerTest < Minitest::Test
+class PeriodStartWorkerTest < ActiveSupport::TestCase
   def setup
-    Sidekiq::Worker.clear_all
     @worker = RodTheBot::PeriodStartWorker.new
-    @game_id = 2023020341 # replace with a real game id
   end
 
-  def test_perform_first_period
-    VCR.use_cassette("nhl_gamecenter_pbp_#{@game_id}") do
-      @feed = HTTParty.get("https://api-web.nhle.com/v1/gamecenter/#{@game_id}/play-by-play")
-
-      play_id = 102
-      play = @feed["plays"].find { |play| play["eventId"].to_i == play_id.to_i }
-
-      @worker.perform(@game_id, play)
-
-      assert_equal 1, RodTheBot::GameStartWorker.jobs.count
-    end
-  end
-
-  def test_second_period
-    game_id = 2023020377
+  test "perform for first period" do
+    game_id = "2023020341"
     VCR.use_cassette("nhl_gamecenter_pbp_#{game_id}") do
-      @feed = HTTParty.get("https://api-web.nhle.com/v1/gamecenter/#{game_id}/play-by-play")
+      play = {
+        "periodDescriptor" => {"number" => 1, "periodType" => "REG"},
+        "eventId" => 102
+      }
 
-      play_id = 123
-      play = @feed["plays"].find { |play| play["eventId"].to_i == play_id.to_i }
+      RodTheBot::GameStartWorker.expects(:perform_async).with(game_id)
+      RodTheBot::Post.expects(:perform_async).never
 
       @worker.perform(game_id, play)
-
-      expected_output = <<~POST
-        🎬 It's time to start the 2nd Period at Canada Life Centre!
-        
-        We're ready for another puck drop between the Hurricanes and the Jets!
-      POST
-      assert_equal expected_output, RodTheBot::Post.jobs.first["args"].first
     end
   end
 
-  def test_perform_ot_period
-    VCR.use_cassette("nhl_gamecenter_pbp_#{@game_id}") do
-      @feed = HTTParty.get("https://api-web.nhle.com/v1/gamecenter/#{@game_id}/play-by-play")
+  test "perform for second period" do
+    game_id = "2023020377"
+    VCR.use_cassette("nhl_gamecenter_pbp_#{game_id}") do
+      play = {
+        "periodDescriptor" => {"number" => 2, "periodType" => "REG"},
+        "eventId" => 123
+      }
 
-      play_id = 449
-      play = @feed["plays"].find { |play| play["eventId"].to_i == play_id.to_i }
+      RodTheBot::GameStartWorker.expects(:perform_async).never
+      RodTheBot::Post.expects(:perform_async).with(regexp_matches(/🎬 It's time to start the 2nd Period/))
 
-      @worker.perform(@game_id, play)
+      @worker.perform(game_id, play)
+    end
+  end
 
-      expected_output = <<~POST
-        🎬 It's time to start the OT Period at PNC Arena!
-        
-        We're ready for another puck drop between the Islanders and the Hurricanes!
-      POST
+  test "perform for overtime period" do
+    game_id = "2023020341"
+    VCR.use_cassette("nhl_gamecenter_pbp_#{game_id}") do
+      play = {
+        "periodDescriptor" => {"number" => 4, "periodType" => "OT"},
+        "eventId" => 449
+      }
 
-      assert_equal expected_output, RodTheBot::Post.jobs.first["args"].first
+      RodTheBot::GameStartWorker.expects(:perform_async).never
+      RodTheBot::Post.expects(:perform_async).with(regexp_matches(/🎬 It's time to start the OT Period/))
+
+      @worker.perform(game_id, play)
+    end
+  end
+
+  test "perform for shootout" do
+    game_id = "2023020341"
+    VCR.use_cassette("nhl_gamecenter_pbp_#{game_id}") do
+      play = {
+        "periodDescriptor" => {"number" => 5, "periodType" => "SO"},
+        "eventId" => 500
+      }
+
+      RodTheBot::GameStartWorker.expects(:perform_async).never
+      RodTheBot::Post.expects(:perform_async).never
+
+      @worker.perform(game_id, play)
     end
   end
 end
