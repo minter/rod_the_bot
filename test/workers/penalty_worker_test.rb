5c6f1a92e026f586112479ce461aecda1485fd92
H. Wade Minter <wade@wademinter.com>
2024-09-30 23:05:00 -0400

    Fix penalty worker test

--- test/workers/penalty_worker_test.rb
+++ test/workers/penalty_worker_test.rb
@@ -1,68 +1,66 @@
-class PenaltyWorkerTest < Minitest::Test
+require "test_helper"
+require "vcr"
+require "sidekiq/testing"
+
+class PenaltyWorkerTest < ActiveSupport::TestCase
   def setup
-    Sidekiq::Worker.clear_all
     @worker = RodTheBot::PenaltyWorker.new
+    ENV["NHL_TEAM_ID"] = "12"  # Assuming this is the Carolina Hurricanes
+    Sidekiq::Testing.fake!  # Enable fake Sidekiq queue
   end
 
-  def test_perform
-    @game_id = 2023020339
-    @play_id = 377
-    ENV["NHL_TEAM_ID"] = "99"
-    VCR.use_cassette("nhl_game_#{@game_id}_gamecenter_pbp_end_of_period_1", allow_playback_repeats: true) do
-      feed = HTTParty.get("https://api-web.nhle.com/v1/gamecenter/#{@game_id}/play-by-play", allow_playback_repeats: true)
-      play = feed["plays"].find { |play| play["eventId"].to_i == @play_id.to_i }
+  def teardown
+    Sidekiq::Worker.clear_all  # Clear Sidekiq queue after each test
+  end
 
-      @worker.perform(@game_id, play)
+  test "perform for various penalty types" do
+    game_ids = ["2023020339", "2023020433"]
 
-      assert_equal 1, RodTheBot::Post.jobs.size
-      expected_output = <<~POST
-        🤩 Kings Penalty!
-        
-        Dylan Strome - Slashing
- 
-        That's a 2 minute Minor penalty at 18:25 of the 1st Period
-      POST
-      assert_equal expected_output, RodTheBot::Post.jobs.first["args"].first
+    game_ids.each do |game_id|
+      VCR.use_cassette("nhl_game_#{game_id}_gamecenter_pbp", allow_playback_repeats: true) do
+        feed = NhlApi.fetch_pbp_feed(game_id)
+        penalty_plays = feed["plays"].select { |play| play["typeDescKey"] == "penalty" }
 
-      # Add assertions here based on what you expect to happen when perform is called
+        penalty_plays.each do |play|
+          assert_difference -> { RodTheBot::Post.jobs.size }, 1 do
+            @worker.perform(game_id, play)
+          end
+
+          # Verify the post content based on the play details
+          expected_content = get_expected_content(play, feed)
+          actual_content = RodTheBot::Post.jobs.last["args"].first
+          assert_match expected_content, actual_content, "Expected content doesn't match actual content for play: #{play["eventId"]}"
+        end
+      end
     end
   end
 
-  def test_perform_penalty_shot
-    @game_id = 2023020433
-    @play_id = 495
-    ENV["NHL_TEAM_ID"] = "12"
-    VCR.use_cassette("nhl_game_#{@game_id}_gamecenter_pbp_penalty", allow_playback_repeats: true) do
-      feed = HTTParty.get("https://api-web.nhle.com/v1/gamecenter/#{@game_id}/play-by-play", allow_playback_repeats: true)
-      play = feed["plays"].find { |play| play["eventId"].to_i == @play_id.to_i }
+  private
 
-      @worker.perform(@game_id, play)
+  def get_expected_content(play, feed)
+    home_team = feed["homeTeam"]
+    away_team = feed["awayTeam"]
+    your_team = (home_team["id"].to_s == ENV["NHL_TEAM_ID"]) ? home_team : away_team
+    their_team = (your_team == home_team) ? away_team : home_team
 
-      assert_equal 1, RodTheBot::Post.jobs.size
-      expected_output = <<~POST
-        🙃 Hurricanes Penalty
-        
-        Pyotr Kochetkov - Throwing Object At Puck
+    players = @worker.send(:build_players, feed)
+    penalized_player = players[play["details"]["committedByPlayerId"]] || players[play["details"]["servedByPlayerId"]]
 
-        That's a penalty shot awarded at 15:39 of the 3rd Period
-      POST
-      assert_equal expected_output, RodTheBot::Post.jobs.first["args"].first
+    penalized_team = (penalized_player && penalized_player[:team_id].to_s == ENV["NHL_TEAM_ID"]) ? your_team : their_team
+    emoji = (penalized_team == your_team) ? "🙃" : "🤩"
 
-      # Add assertions here based on what you expect to happen when perform is called
-    end
-  end
+    period_name = RodTheBot::PeriodFormatter.format_period_name(play["periodDescriptor"]["number"])
 
-  def test_build_players
-    @game_id = 2023020339
-    VCR.use_cassette("nhl_game_#{@game_id}_gamecenter_pbp_end_of_period_1") do
-      feed = HTTParty.get("https://api-web.nhle.com/v1/gamecenter/#{@game_id}/play-by-play")
-      players = @worker.send(:build_players, feed)
+    player_name = penalized_player ? penalized_player[:name] : "Unknown Player"
+    penalty_desc = play["details"]["descKey"].sub(/^ps-/, "").tr("-", " ").titlecase
 
-      assert players.any?
-      assert_equal players.first, [8471214, {team_id: 15, number: 8, name: "Alex Ovechkin"}]
-      assert players.values.all? { |player| player[:team_id] && player[:number] && player[:name] }
-
-      # Add assertions here based on what you expect build_players to return
+    case play["details"]["typeCode"]
+    when "BEN"
+      /#{emoji} #{penalized_team["name"]["default"]} Penalty.*Bench Minor - #{penalty_desc}.*#{play["details"]["duration"]} minute penalty at #{play["timeInPeriod"]} of the #{period_name}/m
+    when "PS"
+      /#{emoji} #{penalized_team["name"]["default"]} Penalty.*#{player_name} - #{penalty_desc}.*penalty shot awarded at #{play["timeInPeriod"]} of the #{period_name}/m
+    else
+      /#{emoji} #{penalized_team["name"]["default"]} Penalty.*#{player_name} - #{penalty_desc}.*#{play["details"]["duration"]} minute #{RodTheBot::PenaltyWorker::SEVERITY[play["details"]["typeCode"]]} penalty at #{play["timeInPeriod"]} of the #{period_name}/m
     end
   end
 end
