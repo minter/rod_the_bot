d405963226191a537e28d0ba517c2c08e3fb195b
H. Wade Minter <wade@wademinter.com>
2024-10-01 22:31:20 -0400

    Refactor final score worker

--- app/workers/rod_the_bot/final_score_worker.rb
+++ app/workers/rod_the_bot/final_score_worker.rb
@@ -24,20 +24,27 @@ module RodTheBot
     private
 
     def format_post(home, visitor, modifier, home_team_is_yours)
+      home_name = home.dig("name", "default") || ""
+      visitor_name = visitor.dig("name", "default") || ""
+      home_score = home.fetch("score", 0)
+      visitor_score = visitor.fetch("score", 0)
+
       post = <<~POST
         Final Score#{modifier}:
 
-        #{visitor.fetch("name", {}).fetch("default", "")} - #{visitor.fetch("score", 0)} 
-        #{home.fetch("name", {}).fetch("default", "")} - #{home.fetch("score", 0)}
+        #{visitor_name} - #{visitor_score} 
+        #{home_name} - #{home_score}
 
         Shots on goal:
 
-        #{visitor.fetch("name", {}).fetch("default", "")}: #{visitor.fetch("sog", 0)}
-        #{home.fetch("name", {}).fetch("default", "")}: #{home.fetch("sog", 0)}
+        #{visitor_name}: #{visitor.fetch("sog", 0)}
+        #{home_name}: #{home.fetch("sog", 0)}
       POST
 
-      if ENV["WIN_CELEBRATION"].present? && (home_team_is_yours && home.fetch("score", 0) > visitor.fetch("score", 0)) || (!home_team_is_yours && home.fetch("score", 0) < visitor.fetch("score", 0))
-        post = "#{ENV["WIN_CELEBRATION"]}\n\n#{post}"
+      if ENV["WIN_CELEBRATION"].present?
+        home_team_won = home_score > visitor_score
+        your_team_won = home_team_is_yours ? home_team_won : !home_team_won
+        post = "#{ENV["WIN_CELEBRATION"]}\n\n#{post}" if your_team_won
       end
 
       post
