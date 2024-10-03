1c515faa6c2efe662f81d4638ecbbf52380448a4
H. Wade Minter <wade@wademinter.com>
2024-10-03 10:32:26 -0400

    There's no reason to wait 15 minutes to post last night's scores

--- app/workers/rod_the_bot/scheduler.rb
+++ app/workers/rod_the_bot/scheduler.rb
@@ -7,7 +7,7 @@ module RodTheBot
     include ActiveSupport::Inflector
 
     def perform
-      RodTheBot::YesterdaysScoresWorker.perform_in(15.minutes)
+      RodTheBot::YesterdaysScoresWorker.perform_async
       if NhlApi.postseason?
         # Postseason
         RodTheBot::PostseasonSeriesWorker.perform_in(16.minutes)
