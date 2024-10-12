a3acd7a880e6de5aae94f76accec81a5fc7eb56c
H. Wade Minter <wade@wademinter.com>
2024-10-12 10:39:22 -0400

    Cut down on characters, so that busy days will post

--- app/workers/rod_the_bot/todays_schedule_worker.rb
+++ app/workers/rod_the_bot/todays_schedule_worker.rb
@@ -7,7 +7,7 @@ module RodTheBot
       schedule = NhlApi.fetch_league_schedule(date: date)
       formatted_schedule = format_schedule(schedule, date)
       time_zone_abbr = Time.zone.tzinfo.abbreviation
-      post_text = "🗓️  Here is the NHL schedule for today (times in #{time_zone_abbr})\n\n#{formatted_schedule}\n\n"
+      post_text = "🗓️  Today's NHL schedule (times #{time_zone_abbr})\n\n#{formatted_schedule}\n\n"
       RodTheBot::Post.perform_async(post_text)
     end
 
@@ -30,7 +30,11 @@ module RodTheBot
     def format_game_time(game)
       if game["gameScheduleState"] == "OK"
         local_time = Time.zone.parse(game["startTimeUTC"])
-        local_time.strftime("%-I:%M %p")
+        if local_time.min.zero?
+          local_time.strftime("%-I %p").sub(/^0/, "")
+        else
+          local_time.strftime("%-I:%M %p")
+        end
       else
         game["gameScheduleState"]
       end
