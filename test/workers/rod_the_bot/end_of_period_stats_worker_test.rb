b71ea45775dae1d64cb94b5b567271f66baa3a56
H. Wade Minter <wade@wademinter.com>
2024-12-14 16:54:11 -0500

    Fix end of period stats worker with new data structure

--- test/workers/rod_the_bot/end_of_period_stats_worker_test.rb
+++ test/workers/rod_the_bot/end_of_period_stats_worker_test.rb
@@ -4,46 +4,46 @@ class RodTheBot::EndOfPeriodStatsWorkerTest < ActiveSupport::TestCase
   def setup
     Sidekiq::Worker.clear_all
     @worker = RodTheBot::EndOfPeriodStatsWorker.new
-    @game_id = 2024020038
-    ENV["NHL_TEAM_ID"] = "52"
+    @game_id = 2024020477
+    ENV["NHL_TEAM_ID"] = "22"
   end
 
   def test_perform
-    VCR.use_cassette("nhl_gamecenter_2024020038_landing") do
+    VCR.use_cassette("nhl_gamecenter_2024020477_landing") do
       period_number = "1st"
 
       @worker.perform(@game_id, period_number)
       assert_equal 3, RodTheBot::Post.jobs.size
       toi_expected_output = <<~POST
-        ⏱️ Time on ice leaders for the Jets after the 1st Period
+        ⏱️ Time on ice leaders for the Oilers after the 1st Period
         
-        J. Morrissey - 7:48
-        N. Pionk - 7:27
-        D. Samberg - 7:19
-        D. DeMelo - 7:18
-        M. Scheifele - 6:20
+        L. Draisaitl - 7:45
+        B. Kulak - 7:28
+        E. Bouchard - 7:20
+        D. Nurse - 6:59
+        C. McDavid - 6:05
       POST
 
       sog_expected_output = <<~POST
-        🏒 Shots on goal leaders for the Jets after the 1st Period
+        🏒 Shots on goal leaders for the Oilers after the 1st Period
         
-        V. Namestnikov - 1
-        R. Kupari - 1
-        N. Pionk - 1
-        N. Ehlers - 1
-        M. Scheifele - 1
+        E. Bouchard - 3
+        V. Podkolzin - 2
+        M. Ekholm - 2
+        R. Nugent-Hopkins - 1
+        L. Draisaitl - 1
       POST
 
       game_stats_expected_output = <<~POST
         📄 Game comparison after the 1st Period
         
-        Faceoffs: MIN - 42.1% | WPG - 57.9%
-        PIMs: MIN - 2 | WPG - 2
-        Blocks: MIN - 4 | WPG - 3
-        Hits: MIN - 7 | WPG - 7
-        Power Play: MIN - 0/1 | WPG - 0/1
-        Giveaways: MIN - 3 | WPG - 7
-        Takeaways: MIN - 3 | WPG - 2
+        Faceoffs: VGK - 42.9% | EDM - 57.1%
+        PIMs: VGK - 2 | EDM - 2
+        Blocks: VGK - 3 | EDM - 11
+        Hits: VGK - 11 | EDM - 6
+        Power Play: VGK - 0/1 | EDM - 1/1
+        Giveaways: VGK - 5 | EDM - 4
+        Takeaways: VGK - 0 | EDM - 1
       POST
 
       assert_equal toi_expected_output, RodTheBot::Post.jobs.second["args"].first
