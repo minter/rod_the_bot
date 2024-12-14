93ede013572d9f2bcc7c66d2c9c08a7e0c106423
H. Wade Minter <wade@wademinter.com>
2024-12-14 17:30:49 -0500

    Fix end of period worker test

--- test/workers/rod_the_bot/end_of_period_worker_test.rb
+++ test/workers/rod_the_bot/end_of_period_worker_test.rb
@@ -3,14 +3,14 @@ class RodTheBot::EndOfPeriodWorkerTest < ActiveSupport::TestCase
   def setup
     Sidekiq::Worker.clear_all
     @end_of_period_worker = RodTheBot::EndOfPeriodWorker.new
-    @game_id = "2023020339"
+    @game_id = "2024020477"
   end
 
   def test_perform
     VCR.use_cassette("nhl_game_#{@game_id}_gamecenter_pbp_end_of_period_1", allow_playback_repeats: true) do
       feed = HTTParty.get("https://api-web.nhle.com/v1/gamecenter/#{@game_id}/play-by-play")
 
-      play_id = 28
+      play_id = 27
       play = feed["plays"].find { |play| play["eventId"].to_i == play_id.to_i }
 
       @end_of_period_worker.perform(@game_id, play)
@@ -23,7 +23,7 @@ class RodTheBot::EndOfPeriodWorkerTest < ActiveSupport::TestCase
   def test_format_post
     VCR.use_cassette("nhl_game_#{@game_id}_gamecenter_pbp_end_of_period_1") do
       feed = HTTParty.get("https://api-web.nhle.com/v1/gamecenter/#{@game_id}/play-by-play")
-      play_id = 28
+      play_id = 27
       play = feed["plays"].find { |play| play["eventId"].to_i == play_id.to_i }
 
       home = feed.fetch("homeTeam", {})
@@ -33,13 +33,13 @@ class RodTheBot::EndOfPeriodWorkerTest < ActiveSupport::TestCase
       expected_output = <<~POST
         🛑 That's the end of the 1st Period!
         
-        Capitals - 1 
-        Kings - 1
-        
+        Golden Knights - 0 
+        Oilers - 4
+ 
         Shots on goal after the 1st Period:
-        
-        Capitals: 5
-        Kings: 11
+ 
+        Golden Knights: 17
+        Oilers: 21
       POST
       assert_equal expected_output, post
     end
